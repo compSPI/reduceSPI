c9811a4d1d3a0d136624621534c1db01602d4665
NicolasLegendre1 <nicolaslegendre963@yahoo.com>
2021-06-22 19:50:29 +0200

    open datasets

--- models/dataset.py
+++ models/dataset.py
@@ -19,6 +19,20 @@ else:
 
 KWARGS = {'num_workers': 1, 'pin_memory': True} if CUDA else {}
 
+PATHS = {}
+PATHS["simulated2d"] = "cryo_sim_128x128.npy"
+PATHS["real_2d30"] = "class2D_30_sort.h5"
+PATHS["real_2d39"] = "class2D_39_sort.h5"
+PATHS["real_2d93"] = "class2D_93_sort.h5"
+PATHS["refine3D"] = "refine3D_180x180_sort.h5"
+PATHS["class3D9090"] = "class3D_90x90_sort.h5"
+PATHS["simulated3d"] = "concat_simulated.npy"
+PATHS["simulated3DNoise"] = "cryo_sim_128x128.npy"
+PATHS["real3d"] = 'data.hdf5'
+PATHS["4points"] = "4points.npy"
+PATHS["4points1"] = "4points1.npy"
+PATHS["4points3d"] = "3d_images.npy"
+
 
 def open_dataset(path, new_size, is_3d):
     """
@@ -41,21 +55,21 @@ def open_dataset(path, new_size, is_3d):
         all_datasets = np.load(path)
     dataset = np.asarray(all_datasets)
     img_shape = dataset.shape
-    N = img_shape[0]
-    Dataset = []
+    n_imgs = img_shape[0]
+    new_dataset = []
     if is_3d:
         dataset = torch.Tensor(dataset)
         dataset = normalization_linear(dataset)
     else:
-        if len(img_shape) == 3:
-            for i in range(N):
-                Dataset.append(np.asarray(Image.fromarray(
+        if img_shape.ndim == 3:
+            for i in range(n_imgs):
+                new_dataset.append(np.asarray(Image.fromarray(
                     dataset[i]).resize([new_size, new_size])))
-        elif len(img_shape) == 4:
-            for i in range(N):
-                Dataset.append(np.asarray(Image.fromarray(
+        elif img_shape.ndim == 4:
+            for i in range(n_imgs):
+                new_dataset.append(np.asarray(Image.fromarray(
                     dataset[i][0]).resize([new_size, new_size])))
-        dataset = torch.Tensor(Dataset)
+        dataset = torch.Tensor(new_dataset)
         dataset = normalization_linear(dataset)
         if len(img_shape) != 4:
             dataset = dataset.reshape(
@@ -129,49 +143,49 @@ SHAPES['4points'] = (1, 128, 128)
 SHAPES['4points1'] = (1, 64, 64)
 SHAPES['4points3d'] = (64, 64, 64)
 
-CONSTANTES = {}
-CONSTANTES['img_shape'] = (1, 128, 128)
-CONSTANTES['is_3d'] = False
-CONSTANTES['with_sigmoid'] = True
-CONSTANTES['OUT_CHANNELS'] = [32, 64]
-CONSTANTES['conv_dim'] = len(CONSTANTES['img_shape'][1:])
-CONSTANTES['dataset_name'] = PATHS['simulated2d']
-CONSTANTES['ENC_KS'] = 4
-CONSTANTES['ENC_STR'] = 2
-CONSTANTES['ENC_PAD'] = 1
-CONSTANTES['ENC_DIL'] = 1
-CONSTANTES['ENC_C'] = 1
-CONSTANTES['DEC_KS'] = 3
-CONSTANTES['DEC_STR'] = 1
-CONSTANTES['DEC_PAD'] = 0
-CONSTANTES['DEC_DIL'] = 1
-CONSTANTES['DEC_C'] = 1
-CONSTANTES['DIS_KS'] = 4
-CONSTANTES['DIS_STR'] = 2
-CONSTANTES['DIS_PAD'] = 1
-CONSTANTES['DIS_DIL'] = 1
-CONSTANTES['DIS_C'] = 1
-CONSTANTES['regularizations'] = ('kullbackleibler')
-CONSTANTES['class_2d'] = 39
-CONSTANTES['weights_init'] = 'xavier'
-CONSTANTES['nn_type'] = 'conv'
-CONSTANTES['beta1'] = 0.9
-CONSTANTES['beta2'] = 0.999
-CONSTANTES['frac_val'] = 0.2
-CONSTANTES['BCE'] = True
-CONSTANTES['DATA_DIM'] = functools.reduce(
-    (lambda x, y: x * y), CONSTANTES['img_shape'])
-CONSTANTES['reconstructions'] = ('bce_on_intensities', 'adversarial')
-CONSTANTES['SKIP_Z'] = False
+CONSTANTS = {}
+CONSTANTS['img_shape'] = (1, 128, 128)
+CONSTANTS['is_3d'] = False
+CONSTANTS['with_sigmoid'] = True
+CONSTANTS['OUT_CHANNELS'] = [32, 64]
+CONSTANTS['conv_dim'] = len(CONSTANTS['img_shape'][1:])
+CONSTANTS['dataset_name'] = PATHS['simulated2d']
+CONSTANTS['enc_ks'] = 4
+CONSTANTS['enc_str'] = 2
+CONSTANTS['enc_pad'] = 1
+CONSTANTS['enc_dil'] = 1
+CONSTANTS['enc_c'] = 1
+CONSTANTS['dec_ks'] = 3
+CONSTANTS['dec_str'] = 1
+CONSTANTS['dec_pad'] = 0
+CONSTANTS['dec_dil'] = 1
+CONSTANTS['dec_c'] = 1
+CONSTANTS['dis_ks'] = 4
+CONSTANTS['dis_str'] = 2
+CONSTANTS['dis_pad'] = 1
+CONSTANTS['dis_dil'] = 1
+CONSTANTS['dis_c'] = 1
+CONSTANTS['regularizations'] = ('kullbackleibler')
+CONSTANTS['class_2d'] = 39
+CONSTANTS['weights_init'] = 'xavier'
+CONSTANTS['nn_type'] = 'conv'
+CONSTANTS['beta1'] = 0.9
+CONSTANTS['beta2'] = 0.999
+CONSTANTS['frac_val'] = 0.2
+CONSTANTS['bce'] = True
+CONSTANTS['dim_data'] = functools.reduce(
+    (lambda x, y: x * y), CONSTANTS['img_shape'])
+CONSTANTS['reconstructions'] = ('bce_on_intensities', 'adversarial')
+CONSTANTS['skip_z'] = False
 
 
 SEARCH_SPACE = {}
-SEARCH_SPACE['encLayN'] = 2
-SEARCH_SPACE['decLayN'] = 2
+SEARCH_SPACE['n_enc_lay'] = 2
+SEARCH_SPACE['n_dec_lay'] = 2
 SEARCH_SPACE['latent_dim'] = 3
 SEARCH_SPACE['batch_size'] = 20
 SEARCH_SPACE['adversarial'] = False
-SEARCH_SPACE['GANLayN'] = 3
+SEARCH_SPACE['n_gan_lay'] = 3
 SEARCH_SPACE['lr'] = 0.001
 SEARCH_SPACE['regu_factor'] = 0.003
 SEARCH_SPACE['lambda_regu'] = 0.2
