e1a8f89daa748a203448bf982f55f86a240e8bdf
Nina Miolane <nmiolane@gmail.com>
2021-10-01 10:24:31 -0700

    Add codecov config

---
+++ tests/test_offline.py
@@ -0,0 +1,10 @@
+"""Test offline models."""
+
+
+class TestOffline:
+    """Test functions from the offline.py module."""
+
+    @staticmethod
+    def test_placeholder():
+        """Test placeholder."""
+        print("Test placeholder.")
